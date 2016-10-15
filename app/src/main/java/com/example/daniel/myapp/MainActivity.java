2df998151ea15ad1219a42828cac31bbad364f17
Daniel Rawana <drawana2@uwo.ca>
2016-10-15 02:00:57 -0400

    Created info entering pade and output to txt file

--- app/src/main/java/com/example/daniel/myapp/MainActivity.java
+++ app/src/main/java/com/example/daniel/myapp/MainActivity.java
@@ -7,21 +7,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class MainActivity extends AppCompatActivity {
+
+   Person person;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Info");
     }
 
     /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
+    public void sendMessage(View view) throws IOException {
         // Do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(AlarmClock.EXTRA_MESSAGE, message);
-        startActivity(intent);
+        person = new Person();
+        PrintWriter out = new PrintWriter(new FileWriter("Info.txt"));
+        EditText textInfo = (EditText) findViewById(R.id.edit_message);
+        person.firstName = textInfo.getText().toString();
+        textInfo = (EditText) findViewById(R.id.edit_message2);
+        person.lastName = textInfo.getText().toString();
+        out.println(person.firstName + " " + person.lastName);
+        textInfo = (EditText) findViewById(R.id.edit_message3);
+        person.healthCardNo = textInfo.getText().toString();
+        out.println(person.healthCardNo);
+        textInfo = (EditText) findViewById(R.id.edit_message4);
+        person.symptoms = textInfo.getText().toString();
+        out.println(person.symptoms);
+        textInfo = (EditText) findViewById(R.id.edit_message5);
+        person.allergies = textInfo.getText().toString();
+        out.println(person.allergies);
+        out.flush();
+        out.close();
+
     }
 }
---
+++ app/src/main/java/com/example/daniel/myapp/Person.java
@@ -0,0 +1,20 @@
+package com.example.daniel.myapp;
+
+/**
+ * Created by Daniel on 2016-10-15.
+ */
+
+public class Person {
+    protected String firstName;
+    protected String lastName;
+    protected String healthCardNo;
+    protected String allergies;
+    protected String symptoms;
+
+    public Person()
+    {
+
+    }
+
+
+}
